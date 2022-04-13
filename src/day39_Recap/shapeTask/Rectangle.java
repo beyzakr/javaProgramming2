72d5084dc6235ff5677fd0255f41522b44c27f34
Aydogan Beyza <bnur933@gmail.com>
2022-04-13 23:36:37 +0300

    day39: Recap

--- src/day39_Recap/shapeTask/Rectangle.java
+++ src/day39_Recap/shapeTask/Rectangle.java
@@ -34,7 +34,26 @@ public class Rectangle extends Shape{
         setWidth(width);
     }
 
+    @Override
+    public double area() {
+        return length * width;
+    }
 
+    @Override
+    public double perimeter() {
+        return 2 *(length + width);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "length=" + length +
+                ", width=" + width +
+                ", area='" + area() + '\'' +
+                ", perimeter='" + perimeter() + '\'' +
+                '}';
+    }
 }
 /*
 Rectangle extends Shape:
