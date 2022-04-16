97f7bf4413d4d1b1a083d9b0539b5bb2f0906f18
Aydogan Beyza <bnur933@gmail.com>
2022-04-16 20:38:08 +0300

    day 40 final keyword

--- src/day40_FinalKeyword/FinalVariable.java
+++ src/day40_FinalKeyword/FinalVariable.java
@@ -2,4 +2,45 @@ package day40_FinalKeyword;
 
 public class FinalVariable {
 
+
+    final String birthday;
+    final static String name;
+
+    static {
+        name = "Batch 25";
+    }
+
+    public FinalVariable(String birthday){
+        this.birthday=birthday;
+    }
+
+    public final static void main(String[] args) {
+        final double pi;
+
+        //pi = 4.14; final variables can not be reassigned
+        // pi = 5.4;
+
+        final String name;
+
+        name="java";
+
+      //  name="wooden spoon";
+        System.out.println(name);
+
+        System.out.println("---------------------------------");
+
+
+        FinalVariable obj = new FinalVariable("May/01");
+        //obj.birthDay = "June/01"; // final variables can not be reassigned
+        System.out.println(obj.birthday);
+
+        System.out.println("------------------------");
+
+       // FinalVariable.name="Python";
+        System.out.println(FinalVariable.name);
+
+
+
+    }
+
 }
