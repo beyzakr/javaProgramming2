0013a6d0e653f181fa0d32326f406d5cffdf1501
Aydogan Beyza <bnur933@gmail.com>
2022-04-16 20:23:38 +0300

    day 40 final keyword

--- src/day40_FinalKeyword/Cat.java
+++ src/day40_FinalKeyword/Cat.java
@@ -1,5 +1,9 @@
 package day40_FinalKeyword;
 
-public class Cat {
+public final class Cat extends Animal {
+
+    public Cat(String name, String breed, char gender, String color, String size, int age) {
+        super(name, breed, gender, color, size, age);
+    }
 
 }
