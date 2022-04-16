b02bb6e3639115e2e4c891340421f204bc3429b7
Aydogan Beyza <bnur933@gmail.com>
2022-04-16 20:44:22 +0300

    day 40 final keyword

--- src/day40_FinalKeyword/ProtectedAccessModifier.java
+++ src/day40_FinalKeyword/ProtectedAccessModifier.java
@@ -2,6 +2,17 @@ package day40_FinalKeyword;
 
 public class ProtectedAccessModifier {
 
+static String name1="Cydeo"; //default access modifier
+protected static String name2="woodenpoon"; // protected
+
+    static void method(){
+
+    }
+
+    protected static void method2(){
+
+    }
+
 
 
 }
