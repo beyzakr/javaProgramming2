cfff74c01147045eebf2dc6799adaf375811ea98
Aydogan Beyza <bnur933@gmail.com>
2022-04-20 01:06:34 +0300

    day 41 throws keyword

---
+++ src/day42_Exceptions/ThrowsKeyword.java
@@ -0,0 +1,26 @@
+package day42_Exceptions;
+
+public class ThrowsKeyword {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.out.println("---------test 1---------- ");
+
+        System.out.println("hello");
+        Thread.sleep(3000);
+        System.out.println("world");
+
+        System.out.println("----------test 1 completed----------");
+
+        System.out.println("---------test 2---------- ");
+
+        System.out.println("hello");
+        Thread.sleep(3000);
+        System.out.println("batch 25");
+
+        System.out.println("----------test 2 completed----------");
+
+
+
+
+    }
+}
