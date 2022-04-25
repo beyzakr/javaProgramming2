0a6aeb95f1bfb1eb477b258a8728b47b792e7352
Aydogan Beyza <bnur933@gmail.com>
2022-04-26 01:31:50 +0300

    day 43 abstract clas & abstract method

--- src/day43_Abstraction/car/CarShop.java
+++ src/day43_Abstraction/car/CarShop.java
@@ -3,6 +3,37 @@ package day43_Abstraction.car;
 public class CarShop {
     public static void main(String[] args) {
 
+ //Car car1 = new Car("cydeo","25","blue",2022,100000);
+        // we can not create object from abstract class, bcs abstract class is not concrete
+
+
+        Honda honda = new Honda("accord","black",2019,30000);
+
+        Audi audi = new Audi("Q7","blue",2020,45000);
+
+        Tesla tesla = new Tesla("Model3","white",2021,60000);
+
+        System.out.println(honda);
+        System.out.println(audi);
+        System.out.println(tesla);
+
+        System.out.println("-------------");
+
+        honda.setColor("red");
+        audi.setColor("pink");
+        tesla.setColor("green");
+
+        honda.setPrice(25000);
+        audi.setPrice(40000);
+        tesla.setPrice(55000);
+
+
+
+        System.out.println(honda);
+        System.out.println(audi);
+        System.out.println(tesla);
+
+
 
 
     }
