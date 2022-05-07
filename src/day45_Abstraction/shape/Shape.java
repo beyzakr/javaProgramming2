6ef4bfa126756134a3bcd9f291dc92373e74d461
Aydogan Beyza <bnur933@gmail.com>
2022-05-08 02:46:57 +0300

    day 45 shape

--- src/day45_Abstraction/shape/Shape.java
+++ src/day45_Abstraction/shape/Shape.java
@@ -17,10 +17,9 @@ public abstract class Shape {
 
     @Override
     public String toString() {
-        return getClass().getSimpleName()+"{" +
+        return
                 "name='" + name + '\'' +
                 "area='" + area() + '\'' +
-                "perimeter='" + perimeter() + '\'' +
-                '}';
+                "perimeter='" + perimeter() + '\'';
     }
 }
